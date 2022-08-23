581198d2c43b1e54a9691c863353d73b7ae63062
63070134 <boomzahrk@gmail.com>
2022-08-23 16:37:25 +0700

    ข้อ 1

---
+++ src/main/java/com/example/week31/Week31Application.java
@@ -0,0 +1,13 @@
+package com.example.week31;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week31Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week31Application.class, args);
+    }
+
+}
